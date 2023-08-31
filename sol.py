34e391c78431189e93d93c826b9a4458fcdf70c2
Andi <andreas.rajchert@dci-student.org>
2023-08-31 14:31:07 +0200

    finish

---
+++ sol.py
@@ -0,0 +1,3 @@
+Original_String = "The Quick BroWnFoX jumps over the laZy Dog!"  
+print(f"String in uppercase: {Original_String.upper()}")
+print(f"String in lowercase: {Original_String.lower()}")  
